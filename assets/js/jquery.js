7981900d2441b1f5869d33f807f87d5fed9b5721
lavisse <chloe.lavisse80@gmail.com>
2018-10-15 15:49:33 +0200

    envoi des fichiers

---
+++ assets/js/jquery.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('#clickMe').click(function(){
+  $('#text').css('font-family', 'Arial');
+  });
+});
+// .css sert à appliquer du css sur l'élément sélectionné. Ici on change la police
